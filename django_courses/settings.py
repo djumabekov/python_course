b426933d176406f8fad0956e732cfd71320c92f9
darkhan.jumabekov@gmail.com <darkhan.jumabekov@gmail.com>
2021-11-04 22:11:56 +0600

    setting

--- django_courses/settings.py
+++ django_courses/settings.py
@@ -107,10 +107,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': os.getenv('DB_NAME'),
-        'USER': 'itvdn',
-        'PASSWORD': 'itvdn',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
